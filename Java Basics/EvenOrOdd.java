ca7407edba63101fe0fb1ed5f28eb82b2083bf4b
Rushikesh Wani <wanirushikeshanil11@gmail.com>
2024-04-21 23:41:40 +0530

    Initialized repository

---
+++ Java Basics/EvenOrOdd.java
@@ -0,0 +1,11 @@
+class EvenOrOdd {
+
+  public static void main(String[] args) {
+    int x = 20;
+    if (x % 2 == 0) {
+      System.out.println("Number is even!");
+    } else {
+      System.out.println("Number is odd!");
+    }
+  }
+}
